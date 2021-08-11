113c966eb6d438a0cb5a16c2b4fc5f7f5781d4cc
Udayraj <udayraj.m@bizamica.com>
2021-08-11 08:15:31 +0530

    isSliding flag implementation complete

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
+let isSliding = false;
 /* Build out functions */
 
 function togglePlay(){
@@ -23,9 +24,28 @@ function updateButton(){
     console.log('Update the button');
 }
 
+function skip() {
+    console.log('Skipping');
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+    if(!isSliding){
+        return;
+    }
+    console.log(this.value);
+}
+
 
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click',skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mouseup',() => isSliding = false));
+ranges.forEach(range => range.addEventListener('mousedown',() => isSliding = true));
+ranges.forEach(range => range.addEventListener('mouseout',() => isSliding = false));
